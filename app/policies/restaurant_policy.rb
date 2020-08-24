93bf90102140cf5827a0db4aa9710ae3bb986e78
Julio Quintana <julioeq29@gmail.com>
2020-08-24 23:41:29 +0100

    added CRUD for APIs

--- app/policies/restaurant_policy.rb
+++ app/policies/restaurant_policy.rb
@@ -8,4 +8,18 @@ class RestaurantPolicy < ApplicationPolicy
   def show?
     true
   end
+
+  def update?
+    # Only restaurant owners can update it
+    record.user == user
+  end
+
+  def create?
+    # Any logged-in user can create a Resto
+    !user.nil?
+  end
+
+  def destroy?
+    update?
+  end
 end
